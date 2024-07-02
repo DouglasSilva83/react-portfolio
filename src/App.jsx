e1c58a0c00637c863aa062ea768121972f01efb7
Douglas Silva <i.douglasdcs@gmail.com>
2024-07-02 00:16:49 -0300

    Att. Experiências.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import styles from "./App.module.css"
 import { About } from "./Components/About/About"
+import { Experience } from "./Components/Experience/Experience"
 import { Hero } from "./Components/Hero/Hero"
 import { Navbar } from "./Components/Navbar/Navbar"
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Hero />
         <About />
+        <Experience />
       </div>
   )
 }
